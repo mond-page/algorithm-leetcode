8a2c84e6f53dabff83f0d8a1282e0e6c268906ea
mond <45928808+mond-page@users.noreply.github.com>
2022-08-05 21:40:49 +0900

    Time: 2 ms (82.18%), Space: 50 MB (17.62%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,15 +1,15 @@
 class Solution {    
     public int jump(int[] nums) {
         int last = 0;
-        int fastIndexValue = 0;
+        int maxJumpValue = 0;
         int count = 0;
         
         for (int start = 0; start < nums.length -1; start++) {
-            fastIndexValue = Math.max(fastIndexValue, start + nums[start]);
+            maxJumpValue = Math.max(maxJumpValue, start + nums[start]);
             
             if (start == last) {
                 count++;
-                last = fastIndexValue;
+                last = maxJumpValue;
             }
         }
         
